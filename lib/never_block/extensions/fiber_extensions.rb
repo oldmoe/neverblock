994840677c236c2ab4058c81a4382f359fcb0637
oldmoe <oldmoe@gmail.com>
2008-08-17 15:37:13 +0300

    documenting fiber extensions

--- lib/never_block/extensions/fiber_extensions.rb
+++ lib/never_block/extensions/fiber_extensions.rb
@@ -1,17 +1,21 @@
 require 'fiber'
 
 class Fiber
+  
+  #Attribute Reference--Returns the value of a fiber-local variable, using either a symbol or a string name. If the specified variable does not exist, returns nil.
   def [](key)
     local_fiber_variables[key]
   end
   
+  #Attribute Assignment--Sets or creates the value of a fiber-local variable, using either a symbol or a string. See also Fiber#[].
   def []=(key,value)
     local_fiber_variables[key] = value
   end
   
+  private
+  
   def local_fiber_variables
     @local_fiber_variables ||= {}
-  end  
-  private :local_fiber_variables
+  end    
 end
 
