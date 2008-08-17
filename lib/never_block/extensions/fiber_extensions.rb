505d63e57f40f40436fa4f8328ccfb541b5b8d29
Ahmed Sobhi <humanzz@hotmail.com>
2008-08-17 16:33:56 +0300

    more documentation

--- lib/never_block/extensions/fiber_extensions.rb
+++ lib/never_block/extensions/fiber_extensions.rb
@@ -1,3 +1,7 @@
+# Author::    Mohammad A. Ali  (mailto:oldmoe@gmail.com)
+# Copyright:: Copyright (c) 2008 eSpace, Inc.
+# License::   Distributes under the same terms as Ruby
+
 require 'fiber'
 
 class Fiber
--- lib/neverblock.rb
+++ lib/neverblock.rb
@@ -1,3 +1,7 @@
+# Author::    Mohammad A. Ali  (mailto:oldmoe@gmail.com)
+# Copyright:: Copyright (c) 2008 eSpace, Inc.
+# License::   Distributes under the same terms as Ruby
+
 $:.unshift File.expand_path(File.dirname(__FILE__))
 
 require 'never_block'
