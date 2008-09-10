baee98db410f44bca3fd4a6305843e170e3d8137
humanzz <humanzz@hotmail.com>
2008-09-10 15:39:05 +0200

    allow marking certain actions to execute in blocking or nonblocking manners overriding the defaults

--- lib/never_block/frameworks/rails.rb
+++ lib/never_block/frameworks/rails.rb
@@ -1,6 +1,4 @@
 require 'neverblock' unless defined?(NeverBlock)
-#require 'actionpack'
-#require 'action_controller'
 
 # Rails tries to protect dispatched actions
 # by wrapping them in a synchronized code
@@ -9,29 +7,59 @@ require 'neverblock' unless defined?(NeverBlock)
 # transform it (without it knowing) to
 # something more subtle
 
-
-=begin
-class ActionController::Dispatcher
-
-  # let's show this guard who is
-  # the man of the house
-  @@guard = Object.new
-
-  # now you synchronize
-  def @@guard.synchronize(&block)
-    # now you don't!
-    block.call
-  end
-end
-=end
-
-
 require 'thread'
-
 # now you synchronize
 class Mutex
   def synchronize(&block)
     # now you don't!
     block.call
+  end
+end
+
+require 'action_controller'
+class ActionController::Base
+
+  # Mark some actions to execute in a blocking manner overriding the default
+  # settings.
+  # Example:
+  #   class UsersController < ApplicationController
+  #     .
+  #     allowblock :index
+  #     .
+  #   end
+  def self.allowblock(*actions)
+    actions.each do |action|
+      class_eval <<-"end_eval"
+        def allowblock_#{action}
+          status = Fiber.current[:neverblock]
+          Fiber.current[:neverblock] = false
+          yield
+          Fiber.current[:neverblock] = status
+        end
+        around_filter :allowblock_#{action}, :only => [:#{action}]
+      end_eval
+    end
+  end
+
+  # Mark some actions to execute in a non-blocking manner overriding the default
+  # settings.
+  # Example:
+  #   class UsersController < ApplicationController
+  #     .
+  #     allowblock :index
+  #     .
+  #   end
+  def self.neverblock(*actions)
+    actions.each do |action|
+      class_eval <<-"end_eval"
+        def neverblock_#{action}
+          status = Fiber.current[:neverblock]
+          Fiber.current[:neverblock] = true
+          yield
+          Fiber.current[:neverblock] = status
+        end
+        around_filter :allowblock_#{action}, :only => [:#{action}]
+      end_eval
+    end
   end
 end
