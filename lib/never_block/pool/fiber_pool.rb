e80ac8a66b9653976f6e412bc855cbda0630aa46
humanzz <humanzz@hotmail.com>
2008-09-21 16:57:03 +0200

    initial tests

--- lib/never_block/pool/fiber_pool.rb
+++ lib/never_block/pool/fiber_pool.rb
@@ -31,46 +31,45 @@ module NeverBlock
     #   end
     # end
     #
-	  class FiberPool
+    class FiberPool
 
-	    # gives access to the currently free fibers
-		  attr_reader :fibers
+      # gives access to the currently free fibers
+      attr_reader :fibers
 
-		  # Prepare a list of fibers 
-		  # that are able to run different
-		  # blocks of code every time
-		  # once a fiber is done with its block
-		  # it attempts to fetch another one
-		  # from the queue. 
-		  def initialize(count = 50)
-			  @fibers,@queue = [],[]
-			  count.times do |i| 
-				  fiber = Fiber.new do |block|
-					  loop do
-						  block.call
-						  unless @queue.empty?
-							  block = @queue.shift
-						  else
-							  block = Fiber.yield @fibers << Fiber.current
-						  end
-					  end
-				  end
-				  fiber[:neverblock] = true
-				  @fibers << fiber
-			  end 
-		  end
+      # Prepare a list of fibers that are able to run different blocks of code
+      # every time. Once a fiber is done with its block, it attempts to fetch
+      # another one from the queue
+      def initialize(count = 50)
+        @fibers,@queue = [],[]
+        count.times do |i|
+          fiber = Fiber.new do |block|
+            loop do
+              block.call
+              unless @queue.empty?
+                block = @queue.shift
+              else
+                block = Fiber.yield @fibers << Fiber.current
+              end
+            end
+          end
+          fiber[:neverblock] = true
+          @fibers << fiber
+        end
+      end
 
-		  # If there is an available fiber
-		  # use it, otherwise, leave it to linger in a queue
-		  def spawn(evented = true, &block)
-			  if fiber = @fibers.shift
-			    fiber[:neverblock] = evented
-				  fiber.resume(block)
-			  else
-				  @queue << block
-			  end
-			  self # we are keen on hiding our queue 
-		  end
-	  end
-	end
-end
+      # If there is an available fiber use it, otherwise, leave it to linger
+      # in a queue
+      def spawn(evented = true, &block)
+        if fiber = @fibers.shift
+          fiber[:neverblock] = evented
+          fiber.resume(block)
+        else
+          @queue << block
+        end
+        self # we are keen on hiding our queue
+      end
+
+    end # FiberPool
+  end # Pool
+end # NeverBlock
+
---
+++ spec/fiber_pool_spec.rb
@@ -0,0 +1,59 @@
+require '../lib/neverblock'
+
+describe NeverBlock::Pool::FiberPool do
+  before(:each) do
+    @fiber_pool = NeverBlock::Pool::FiberPool.new(10)
+  end
+
+  it "should have all fibers ready and an empty queue initially" do
+    @fiber_pool.fibers.length.should == 10
+    @fiber_pool.instance_variable_get(:@queue).length.should == 0
+  end
+
+  it "should have fibers with :neverblock fiber variable set to true" do
+    @fiber_pool.fibers.each {|f| f[:neverblock].should == true}
+  end
+
+  it "should process a new block if there are available fibers" do
+    x = false
+    @fiber_pool.spawn {x  = true}
+    x.should == true
+  end
+
+  it "should queue requests if requests are more than fibers" do
+    progress = Array.new(15, false)
+    fibers = []
+    @fiber_pool.fibers.each {|f| fibers << f}
+    10.times do |i|
+      @fiber_pool.spawn {Fiber.yield; progress[i] = true}
+    end
+    @fiber_pool.fibers.length.should == 0
+    @fiber_pool.instance_variable_get(:@queue).length.should == 0
+
+    #it should now queue
+     (10..14).each {|i| @fiber_pool.spawn {progress[i] = true}}
+    @fiber_pool.fibers.length.should == 0
+    @fiber_pool.instance_variable_get(:@queue).length.should == 5
+
+    #resume the first fiber, this should also process the queued requests
+    fibers[0].resume
+    @fiber_pool.fibers.length.should == 1
+    @fiber_pool.instance_variable_get(:@queue).length.should == 0
+    [0,*10..14].each {|i| progress[i].should == true}
+     (1..9).to_a.each {|i| progress[i].should == false}
+    fibers_count = 1
+     (1..9).to_a.each do |i|
+      fibers[i].resume
+      fibers_count = fibers_count + 1
+      progress[i].should == true
+      @fiber_pool.fibers.length.should == fibers_count
+    end
+    @fiber_pool.instance_variable_get(:@queue).length.should == 0
+  end
+
+  after(:each) do
+    @fiber_pool = nil
+  end
+
+end
+
