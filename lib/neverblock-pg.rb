bf02db6535cc55f01b09c63e6bc1bcf1efa789a0
modsaid <mahmoud@modsaid.com>
2009-01-04 16:04:28 +0200

    required include

--- lib/neverblock-pg.rb
+++ lib/neverblock-pg.rb
@@ -1,5 +1,5 @@
 $:.unshift File.expand_path(File.dirname(__FILE__))
-#require 'neverblock'
+require 'neverblock'
 require 'never_block/db/fibered_db_connection'
 require 'never_block/db/pooled_db_connection'
 require 'never_block/db/fibered_postgres_connection'
